3b1ffa25e33f5ebf86ae44825a61b9f660fb4bfb
freshcodeAlice <freshjs.ofte@gmail.com>
2024-01-23 11:42:01 +0200

    alter

---
+++ alter.sql
@@ -0,0 +1,19 @@
+CREATE TABLE dogs (
+    name varchar(300),
+    breed varchar(300),
+    color varchar(300)
+);
+
+INSERT INTO dogs (name, breed, color) VALUES 
+('Bark', 'None', 'Red'), ('Doggi', 'Alabai', 'Black'), ('Bark', 'None', 'Red');
+
+ALTER TABLE dogs
+-- далі іде дія
+ADD COLUMN id serial;
+
+
+ALTER TABLE dogs
+DROP COLUMN id;
+
+ALTER TABLE dogs
+ADD CONSTRAINT "dogs_pkey" PRIMARY KEY (id);
