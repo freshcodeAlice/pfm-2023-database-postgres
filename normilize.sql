81e85880a16c6dcd7d8b836777f8768558e61ffc
freshcodeAlice <freshjs.ofte@gmail.com>
2024-02-02 11:58:33 +0200

    4NF

--- normilize.sql
+++ normilize.sql
@@ -19,6 +19,18 @@
 - всі інші (не ключові) стовпці залежать від ключа.
 ЯКЩО ключ составний, то всі неключові стовпці залежать від ВСІХ ключових стовпів
 
+
+3NF
+(неключові)
+- БД вже в 2NF
+- немає транзитивних залежностей (неключові стовпці не намагаються бути ключовими)
+
+BCNF (посилена 3NF)
+(составний ключ ПРАВИЛЬНИЙ)
+- БД вже в 3NF
+- жоден з стовпців составного ключа не залежить від неключового стовпця
+
+
 */
 
 
@@ -126,3 +138,119 @@ CREATE TABLE employees (
     name varchar(300),
     deparment varchar(300) REFERENCES deparments(name)
 ); -- 3NF
+
+
+--- Нормальна форма Бойса-Кодда (BCNF)
+
+/*
+Є викладачі, студенти, предмети
+(teachers, students, subjects)
+1 викладач - 1 предмет
+у студента може бути багато дисциплін
+1 предмет слухає багато студентів
+
+teachers n:1 subjects
+students m:n subjects
+students m:n teachers
+
+*/
+
+
+CREATE TABLE students (
+    id serial PRIMARY KEY
+);
+
+INSERT INTO students VALUES (1), (2);
+
+CREATE TABLE teachers (
+    id serial PRIMARY KEY
+);
+
+INSERT INTO teachers VALUES (1), (2);
+
+CREATE TABLE students_to_teachers (
+    teacher_id int REFERENCES teachers(id),
+    student_id int REFERENCES students(id),
+    subject varchar(32),
+    PRIMARY KEY(teacher_id, student_id)
+);  -- Частина составного первинного ключа ЗАЛЕЖИТЬ від неключового стовпця (teacher -> subject)
+
+INSERT INTO students_to_teachers (student_id, teacher_id, subject) VALUES
+(1, 1, 'biology'),
+(1, 2, 'math'),
+(2, 1, 'math'),
+(2, 2, 'phisics');
+
+
+-- Рішення:
+
+CREATE TABLE subjects (
+    name varchar(200) PRIMARY KEY
+);
+
+-- зміна teachers
+
+CREATE TABLE teachers (
+    id serial PRIMARY KEY,
+    subject varchar(200) REFERENCES subjects(name)
+);
+
+-- students -> teachers (які вже прив'язані до свого предмета)
+
+CREATE TABLE students_to_teachers (
+    teacher_id int REFERENCES teachers(id),
+    student_id int REFERENCES students(id),
+    PRIMARY KEY (teacher_id, student_id)
+);
+
+
+/*
+Проектування БД має спиратись на принцип Single source of truth (єдине джерело істини)
+Всі факти - первинні (тобто такі, які неможливо вивести з інших фактів)
+
+*/
+
+
+/*
+Ресторани роблять піци
+Піци від ресторанів розвозять служби доставки
+
+
+
+
+*/
+
+CREATE TABLE restaurants (
+    id serial PRIMARY KEY
+);
+
+CREATE TABLE delivery_services (
+    id serial PRIMARY KEY
+);
+
+CREATE TABLE restaurants_to_deliveries (
+    restaurant_id int REFERENCES restaurants,
+    delivery_id int REFERENCES delivery_services,
+    pizza_type varchar(64) NOT NULL,
+    PRIMARY KEY (restaurant_id, delivery_id)
+);
+
+INSERT INTO restaurants_to_deliveries VALUES
+(1, 1, 'peperoni'),
+(1, 1, 'sea'),
+(1, 1, '4 chease'),
+(1, 1, 'hawaii'),
+(1, 2, 'peperoni'),
+(1, 2, 'sea'),
+(1, 2, '4 chease'),
+(2, 1, 'pepponi'),
+(2, 1, 'special');
+
+-- Рішення?
+
+CREATE TABLE pizza_to_restaurants (
+    pizza_type varchar(200),
+    restaurant_id int REFERENCES restaurants(id)
+);
+
+-- Тепер з таблиці restaurants_to_deliveries можна прибрати піци, і лишити тільки відповідність між ресторанами та службами доставки
